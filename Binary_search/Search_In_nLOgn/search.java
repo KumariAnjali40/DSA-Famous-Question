eddca17d9efe69771e722f48123df31fed6eef39
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-11 12:02:48 +0530

    search in logn

---
+++ Binary_search/Search_In_nLOgn/search.java
@@ -0,0 +1,28 @@
+
+class search {
+  public static void main(String[] args) {
+    int n=5;
+    int [] arr ={1,2,3, 5,6};
+
+    int k=4;
+
+    int ans=binarySearch(arr,n,k);
+    System.out.println(ans);
+}
+public static int binarySearch(int []arr,int n,int k){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]==k){
+            return mid;
+        }else if(arr[mid]<k){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return -1;
+  }
+    
+}
