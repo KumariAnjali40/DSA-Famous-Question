8f62e0c67e74ae24e810b60c4b830ab470bbb696
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-13 18:55:14 +0530

    bottom up approch robbing house 1

--- Dynamic-programming/House_Robber1/houseRobber.java
+++ Dynamic-programming/House_Robber1/houseRobber.java
@@ -26,11 +26,14 @@ public class houseRobber {
         int n=4;
         int [] arr ={1,2,3,1} ; 
 
-        int [] dp=new int [n];
+        int [] dp=new int [n+1];
         Arrays.fill(dp,-1);
 
         int ans=rec(arr,n,0,dp);
         System.out.println(ans);
+
+        int ans2=bottomUp(arr,n,dp);
+        System.out.println(ans2);
     }
 
     public static int rec(int [] arr, int n, int index,int [] dp){
@@ -48,4 +51,28 @@ public class houseRobber {
 
         return dp[index];
     }
+
+
+    //let come to bottom up approach .
+
+    // dp[i]=mex stolen memory till house i.
+    // dp[0]=max profit till house 0 means 0;
+    public static int bottomUp(int [] arr, int n,int [] dp){
+        dp[0]=0;
+        dp[1]=arr[0];
+
+        for(int i=2;i<=n;i++){
+            int steal=arr[i-1]+dp[i-2];
+            int skip=dp[i-1];
+
+            dp[i]=Math.max(steal,skip);
+        }
+
+        return dp[n];
+
+    }
+    
 }
+
+
+
