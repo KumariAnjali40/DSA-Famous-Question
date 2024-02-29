378f09b7707caf4598b1754e0b0ab3d10eb0bbe5
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-29 14:43:47 +0530

    display

--- Recurssion/printArray/printArray.java
+++ Recurssion/printArray/printArray.java
@@ -2,15 +2,22 @@ public class printArray {
 public static void main(String[] args) {
     int [] arr = {10,20,30,40,60};
 
-    display(arr,0);
+    display(arr,arr.length-1);
 }
 
-public static void display(int [] arr,int idx){
-    if(arr.length==idx){
+// public static void display(int [] arr,int idx){
+//     if(arr.length==idx){
+//         return ;
+//     }
+
+//     System.out.println(arr[idx]);
+//     display(arr, idx+1);
+// }
+public static void display(int [] arr, int idx){
+    if(idx<0){
         return ;
     }
-
+    display(arr, idx-1);
     System.out.println(arr[idx]);
-    display(arr, idx+1);
 }
 }
