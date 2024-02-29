378434b8b521f0bba5f130cf20462b29bdf34913
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-02-29 15:31:50 +0530

    max in array

---
+++ Recurssion/maxOfArray/maxOfArray.java
@@ -0,0 +1,24 @@
+// package Recurssion.maxOfArray;
+
+public class maxOfArray {
+    public static void main(String[] args) {
+        int [] arr = {10,20,40,60,30};
+
+
+       int ans= findMax(arr,0);
+       System.out.println(ans);
+    }
+
+    public static int findMax(int [] arr, int idx){
+        if(idx==arr.length-1){
+            return arr[idx];
+        }
+
+        int misa=findMax(arr, idx+1);
+        if(misa>arr[idx]){
+            return misa;
+        }else{
+            return arr[idx];
+        }
+    }
+}
