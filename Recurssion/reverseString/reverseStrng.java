d2771d897cce92a9646bb74c6a7f7e3f3863394d
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-01 20:49:00 +0530

    reverseString

---
+++ Recurssion/reverseString/reverseStrng.java
@@ -0,0 +1,19 @@
+// package Recurssion.reverseString;
+
+public class reverseStrng {
+    public static void main(String[] args) {
+        String s = "abcdef";
+
+        String rev = reverseString(s);
+
+        System.out.println(rev);
+    }
+
+    public static String reverseString(String s){
+        if(s.isEmpty() || s.length()==1){
+            return s;
+        }else{
+            return reverseString(s.substring(1))+s.charAt(0);
+        }
+    }
+}
